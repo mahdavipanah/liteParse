d8fbfbb433d6771b1bb0d993b6cf0c3d4d4f8cbc
Hamidreza.M <micheal.scorfiled@gmail.com>
2012-04-25 05:14:46 +0430

    liteParse bin file

---
+++ bin/litepar.js
@@ -0,0 +1,48 @@
+#!/usr/bin/env node
+
+var program = require('commander'),
+    lp = require('../index.js');
+require('colors');
+program.version('1.0.0')
+       .usage('parse <file> <data file> [options]')
+       .option('-s, --show','Show the parsed string')
+       .option('-e, --encoding <value>','File parsing encoding') 
+       .option('-t, --tofile <value>','Save the parsed file to another file')
+       .option('-l, --leftdelim <value>','Set the left delimeter')
+       .option('-r, --rightdelim <value>','Set the right delimeter')
+;
+
+program.command('parse <file> datafile <datafile>')
+       .description('Parse a file with the JSON data of the given data file')
+       .action(function(file,datafile){ 
+       	   if(program.show === undefined)
+       	   	program.show = false;
+       	   if(!program.tofile)
+       	   	program.show = true;
+           var result = '';        
+           if(program.leftdelim)   
+           	lp.setDelimeter(program.leftdelim,lp.rightDelim());
+           if(program.rightDelim)
+           	lp.setDelimeter(lp.leftDelim(),program.rightdelim)
+           try {
+               result = lp.fileParsing({               
+                 "fileName" : file,                                
+                 "dataFile" : datafile,
+                 "return" : program.show,
+                 "toFile" : program.tofile,
+                 "encoding" : program.encoding
+               });
+           } catch(e) {
+               console.log(e.message.red);
+               return false;
+           }
+           if(result === true)
+           	result = file.inverse + ' parsed with the JSON data of the file '.green+datafile.inverse;
+           if(program.tofile)
+           	result += ' to '.green+'---->'.rainbow+' '+program.tofile.inverse;
+           console.log(result);
+       })
+;            
+
+program.parse(process.argv);    
+
